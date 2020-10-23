69cda28411e52786eeeb6e711b1d8f9089c52c18
Felipe Merchan <felipe-sc@hotmail.es>
2020-10-23 00:47:02 -0500

    Modulo 3: Evitar codigo duplicado

---
+++ prevent-duplication/src/js/components/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import data from './data.json'
+import Loader from './Loader'
+import logo from '../../images/platzi.png'
+import video from '../../video/que-es-core.mp4'
+
+import '../../sass/sass.scss'
+import '../../less/less.less'
+import '../../stylus/stylus.styl'
+
+function App() {
+  const [loaderList, setLoaderList] = useState([])
+  function handleClick() {
+    setLoaderList(data.loaders)
+  }
+
+  return (
+    <div>
+      <p className="sass">Esto es sass</p>
+      <p className="less">Esto es less</p>
+      <p className="stylus">Esto es stylus</p>
+      <p className="post-css">Esto es PostCSS</p>
+      <video src={video} width={360} controls poster={logo}></video>
+      <p>
+        Qué linda aplicación en React.js
+        <img src={logo} width="40" />
+      </p>
+      <ul>
+        {
+          loaderList.map(loader => <Loader {...loader} key={loader.id} />)
+        }
+      </ul>
+      <button onClick={handleClick}>Mostrar lo aprendido hasta el momento</button>
+    </div>
+  )
+}
+
+export default App
